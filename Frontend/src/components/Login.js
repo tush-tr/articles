ed09e7626297222428ccc4d936fd5d995848b184
Arish Rehman Khan <arish.rehman.khan@gmail.com>
2021-04-19 15:40:48 +0530

    change style of login and signup component

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -1,24 +1,25 @@
 import React from "react";
 import '../styles/login_style.css';
+
 function Login() {
-    return (
-        <div className="full-screen-container">
-          <div className="login-container">
-            <h3 className="login-title">Welcome</h3>
-            <form>
-              <div className="input-group">
-                <label>Email</label>
-                <input type="email" />
-              </div>
-              <div className="input-group">
-                <label>Password</label>
-                <input type="password" />
-              </div>
-              <button type="submit" className="login-button">Sign In</button>
-            </form>
+  return (
+    <div className="full-screen-container">
+      <div className="login-container">
+        <h3 className="login-title">Welcome</h3>
+        <form>
+          <div className="input-group">
+            <label>Email</label>
+            <input type="email" />
           </div>
-        </div>
-      );
-    }
-    
-    export default Login;
+          <div className="input-group">
+            <label>Password</label>
+            <input type="password" />
+          </div>
+          <button type="submit" className="login-button">Sign In</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- Frontend/src/components/Signup.js
+++ Frontend/src/components/Signup.js
@@ -1,28 +1,29 @@
 import React from "react";
 import '../styles/login_style.css';
+
 function Signup() {
-    return (
-        <div className="full-screen-container">
-          <div className="signup-container">
-            <h3 className="signup-title">Create your account</h3>
-            <form>
-                <div className="input-group">
-                <label>Name</label>
-                <input type="text" />
-                </div>
-              <div className="input-group">
-                <label>Email</label>
-                <input type="email" />
-              </div>
-              <div className="input-group">
-                <label>Password</label>
-                <input type="password" />
-              </div>
-              <button type="submit" className="signup-button">Sign Up</button>
-            </form>
+  return (
+    <div className="full-screen-container">
+      <div className="signup-container">
+        <h3 className="signup-title">Create your account</h3>
+        <form>
+          <div className="input-group">
+            <label>Name</label>
+            <input type="text" />
+            </div>
+          <div className="input-group">
+            <label>Email</label>
+            <input type="email" />
           </div>
-        </div>
-      );
-    }
+          <div className="input-group">
+            <label>Password</label>
+            <input type="password" />
+          </div>
+          <button type="submit" className="signup-button">Register</button>
+        </form>
+      </div>
+    </div>
+  );
+}
     
-    export default Signup;
+export default Signup;
