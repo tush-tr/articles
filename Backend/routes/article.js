c099e237a5767b0e8dd1d276e7000003d0dab20e
outofbound4 <gaurav.kr109@gmail.com>
2021-04-19 14:51:31 +0530

    Added rout for report controller

--- Backend/routes/article.js
+++ Backend/routes/article.js
@@ -5,5 +5,6 @@ const ArticleController = require("../controllers/ArticleController");
 router.post("/", ArticleController.saveArticle);
 router.post("/like", ArticleController.like);
 router.post("/comment", ArticleController.comment);
+router.post("/report", ArticleController.report);
 router.get("/:id", ArticleController.getArticle);
 module.exports = router;
