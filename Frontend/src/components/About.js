e58dd9bb35c31cd52e31687c931278478d8e17f7
Aayushi <aayushirawat47@gmail.com>
2021-04-23 01:53:31 +0530

    New css file added for About.js

--- Frontend/src/components/About.js
+++ Frontend/src/components/About.js
@@ -1,11 +1,11 @@
 
 import React from 'react';
-import '../styles/login_style.css';
+import '../styles/about-style.css';
 
 function About() {
   return (
     <div className="about-container">
-      <h1>ReaderSpot</h1><br/>
+      <h1 >ReaderSpot</h1><br/>
       <h3>Read. Think. Contribute.</h3><br/>
       <p>
       A platform for digital publishing of articles. It’s an environment where everyone can contribute their thoughts and ideas. Readers can find insightful articles curated according to their interests.An environment where readers and writers can come together and lead to discovery and growth.
@@ -15,3 +15,4 @@ function About() {
 }
 
 export default About
+
