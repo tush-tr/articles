a45ad28c9202adcdad5f2ecbec30cdf068b36093
Arish Rehman Khan <arish.rehman.khan@gmail.com>
2021-04-25 16:24:53 +0530

    connect AdminPublishedArticles component to backend

--- Frontend/src/components/AdminPublishedArticles.js
+++ Frontend/src/components/AdminPublishedArticles.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
+import api from "../helpers/api";
+import { UserContext } from "../contexts/UserContext";
+import { useHistory } from "react-router";
+import Moment from "react-moment";
 
 const AdminPublishedArticles = () => {
+    
+  const [user, setUser] = useContext(UserContext);
+
+  const [ publishedArticles, setPublishedArticles ] = useState([]);
+
+  useEffect(() => {
+    getPublishedArticles();
+  }, []);
+
+  const history = useHistory();
+
+  const getPublishedArticles = async () => {
+    const res = await api.get(`/admin/published-articles`, {
+      headers: { "auth-token": user.admin_token },
+    });
+    if (res.data.status === 1) {
+        setPublishedArticles(res.data.data);
+    }
+  };
+
+  const openArticle = (artilcleId) => {
+      history.push(`/article/${artilcleId}`);
+  }
   return (
     <div id="content-wrapper" class="d-flex flex-column">
       <div class="content">
@@ -11,17 +38,23 @@ const AdminPublishedArticles = () => {
           <table class="table">
             <thead class="thead-dark">
               <tr>
-                <th scope="col">#</th>
+                <th scope="col">Id</th>
                 <th scope="col">Title</th>
-                <th scope="col">DatePublished</th>
+                <th scope="col">Date Published</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Title</td>
-                <td>Date</td>
-              </tr>
+                {
+                    publishedArticles && publishedArticles.map((article) => {
+                        return (
+                            <tr key={article._id} onClick={() => openArticle(article._id)}>
+                              <th scope="row">{article._id}</th>
+                              <td>{article.title}</td>
+                              <td><Moment format="MMM DD, YYYY h:m a">{article.publishDate}</Moment></td>
+                            </tr>
+                        );
+                    })
+                }
             </tbody>
           </table>
         </div>
